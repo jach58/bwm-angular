9c2d695800b7fdc7a8d546f1a29f475ce0b4a07f
Square <jach58@hotmail.com>
2018-11-02 13:39:15 -0600

    Se agrego los componentes rental-list, rental-detail ademas de rental service y rental model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RentalModule } from './rental/rental.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'rentals', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [RouterModule.forRoot(routes), BrowserModule, RentalModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/rental/rental.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'bwm-rental',
+  templateUrl: './rental.component.html',
+  styleUrls: ['./rental.component.scss']
+})
+export class RentalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
