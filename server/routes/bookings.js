89f49b2164c91cdff5def3da60821ec1d8548c13
Square <jach58@hotmail.com>
2018-11-06 18:58:57 -0600

    Se agregan los endpoint de administracion de rental y booking y se agrega el borrado de rental

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const UserCtrl = require('../controllers/user');
 const BookingCtrl = require('../controllers/booking');
 
-router.post('', UserCtrl.authMiddleware, BookingCtrl.createBoking);
+router.post('', UserCtrl.authMiddleware, BookingCtrl.createBooking);
+router.get('/manage', UserCtrl.authMiddleware, BookingCtrl.getUserBookings);
 
 module.exports = router;
